81e6142149d3078b323c88a7fc7cbcd1b9d495d2
Mahyad <saedpanahmahyad@gmail.com>
2026-08-20 01:22:34 +0330

    initial commit

---
+++ taxonomy.py
@@ -0,0 +1,53 @@
+CORE_ROLES = {
+    "data_scientist",
+    "data_analyst",
+    "machine_learning_engineer",
+    "ai_engineer",
+}
+
+
+ADJACENT_ROLES = {
+    "bi_analyst",
+    "business_analyst",
+    "data_engineer",
+    "llm_engineer",
+    "ai_specialist",
+    "nlp_engineer",
+}
+
+
+IRRELEVANT_ROLES = {
+    "other",
+    "content_creator",
+    "designer",
+}
+
+
+GENERATIVE_AI_TOOLS = {
+    "midjourney",
+    "flux",
+    "qwen-image",
+    "nano_banana",
+    "kling",
+    "veo",
+    "runway",
+    "hailuo",
+    "seedance",
+    "suno",
+    "elevenlabs",
+    "comfyui",
+}
+
+
+SKILL_CATEGORY_OVERRIDE = {
+    "midjourney": "generative_ai_tool",
+    "flux": "generative_ai_tool",
+    "kling": "generative_ai_tool",
+    "veo": "generative_ai_tool",
+    "runway": "generative_ai_tool",
+    "hailuo": "generative_ai_tool",
+    "seedance": "generative_ai_tool",
+    "suno": "generative_ai_tool",
+    "elevenlabs": "generative_ai_tool",
+    "comfyui": "generative_ai_tool",
+}
